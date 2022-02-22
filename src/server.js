77103fdf08bbdf355903e4d5ae53484f94de9606
Mhsalameh <mohsalameh1@gmail.com>
2022-02-22 19:03:33 +0200

    added json and cors

--- src/server.js
+++ src/server.js
@@ -6,15 +6,13 @@ const logger = require('./middleware/logger.js')
 const validator = require('./middleware/validator.js')
 const notFound=require('./error-handlers/404.js')
 const errorHandler=require('./error-handlers/500.js')
+const cors = require('cors')
 
-const start = (port) => {
-  app.listen(port, () => {
-    console.log(`listening to port ${port}`);
-  });
-};
+
 app.use(logger)
 
-
+app.use(express.json())
+app.use(cors())
 
 app.get('/person',validator,(req,res)=>{
     // console.log(req.reqName)
@@ -24,6 +22,12 @@ app.get('/person',validator,(req,res)=>{
 
 app.use('*',notFound);
 app.use(errorHandler)
+
+const start = (port) => {
+  app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+  });
+};
 
 module.exports = {
   app: app,
